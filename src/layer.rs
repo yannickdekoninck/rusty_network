31d1e5c115cebaa938b96e0c306c0db27bf98136
Yannick De Koninck <yannick.dekoninck@gmail.com>
2022-10-15 11:54:07 +0200

    Serializing and deserializing softmax

--- src/layer.rs
+++ src/layer.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
 use crate::tensor::{helper::TensorShape, Tensor};
 
 pub mod convolutional;
@@ -5,9 +9,22 @@ pub mod fully_connected;
 pub mod max_pool;
 pub mod softmax;
 
+#[derive(Serialize, Deserialize)]
+pub enum SerializedLayer {
+    SerialConvolutionalLayer(HashMap<String, Vec<u8>>),
+    SerialFullyConnectedLayer(HashMap<String, Vec<u8>>),
+    SerialMaxPoolLayer(HashMap<String, Vec<u8>>),
+    SerialSoftMaxLayer(HashMap<softmax::SoftMaxSerialKeys, Vec<u8>>),
+}
+
 pub trait Layer {
     fn forward(self: &Self, input: &Tensor, output: &mut Tensor);
     fn get_input_shape(self: &Self) -> TensorShape;
     fn get_output_shape(self: &Self) -> TensorShape;
     fn get_name(self: &Self) -> String;
+    fn get_serialized(self: &Self) -> SerializedLayer;
+    fn load_from_serialized(
+        self: &mut Self,
+        serial_data: SerializedLayer,
+    ) -> Result<(), &'static str>;
 }
--- src/layer/softmax.rs
+++ src/layer/softmax.rs
@@ -1,6 +1,19 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::Eq;
+use std::collections::HashMap;
+
 use crate::layer::Layer;
 use crate::tensor::helper::TensorShape;
 use crate::tensor::Tensor;
+
+use super::SerializedLayer;
+
+// using an enum for defining keys in serialized data
+#[derive(Serialize, Deserialize, Hash, Eq, PartialEq)]
+pub enum SoftMaxSerialKeys {
+    Shape,
+    Name,
+}
 
 pub struct SoftmaxLayer {
     shape: TensorShape,
@@ -15,6 +28,31 @@ impl SoftmaxLayer {
         };
 
         return Ok(cl);
+    }
+
+    fn load_from_serialized_data(
+        self: &mut Self,
+        data: HashMap<SoftMaxSerialKeys, Vec<u8>>,
+    ) -> Result<(), &'static str> {
+        // Check the correct keys are present
+        if !data.contains_key(&SoftMaxSerialKeys::Name) {
+            return Err("Data does not contain --name-- key");
+        }
+        if !data.contains_key(&SoftMaxSerialKeys::Shape) {
+            return Err("Data does not contain --shape-- key");
+        }
+
+        let shape_data = data
+            .get(&SoftMaxSerialKeys::Shape)
+            .expect("Cannot access shape data");
+        self.shape = bincode::deserialize(shape_data).expect("Cannot deserialize shape data");
+
+        let name_data = data
+            .get(&SoftMaxSerialKeys::Name)
+            .expect("Cannot access name data");
+        self.name = bincode::deserialize(name_data).expect("Cannot deserialize name data");
+
+        return Ok(());
     }
 }
 
@@ -33,5 +71,65 @@ impl Layer for SoftmaxLayer {
 
     fn get_name(self: &Self) -> String {
         return self.name.clone();
+    }
+    fn get_serialized(self: &Self) -> SerializedLayer {
+        // Create empty map
+        let mut serial_data: HashMap<SoftMaxSerialKeys, Vec<u8>> = HashMap::new();
+        // Serialize elements that need to be serialized
+        let serial_shape = bincode::serialize(&self.shape).unwrap();
+        let serial_name = bincode::serialize(&self.name).unwrap();
+        // Add to hashmap
+        serial_data.insert(SoftMaxSerialKeys::Shape, serial_shape);
+        serial_data.insert(SoftMaxSerialKeys::Name, serial_name);
+        // Return wrapped in softmax layer
+        return SerializedLayer::SerialSoftMaxLayer(serial_data);
+    }
+
+    fn load_from_serialized(
+        self: &mut Self,
+        serial_data: SerializedLayer,
+    ) -> Result<(), &'static str> {
+        // Unwrapping the serialized layer and checking it is the correct type
+        match serial_data {
+            SerializedLayer::SerialSoftMaxLayer(data) => {
+                return self.load_from_serialized_data(data);
+            }
+            _ => {
+                return Err("Layer type does not match soft max type");
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_serialize_deserialize() {
+        let sml = SoftmaxLayer::new(
+            TensorShape {
+                di: 5,
+                dj: 4,
+                dk: 3,
+            },
+            &String::from("Softmax layer"),
+        )
+        .unwrap();
+        let serialized_sml = sml.get_serialized();
+
+        let mut sml2 =
+            SoftmaxLayer::new(TensorShape::new(1, 1, 1), &String::from("Nothing")).unwrap();
+        assert!(sml2.load_from_serialized(serialized_sml).is_ok());
+
+        assert_eq!(sml2.name, String::from("Softmax layer"));
+        assert_eq!(
+            sml2.shape,
+            TensorShape {
+                di: 5,
+                dj: 4,
+                dk: 3,
+            },
+        );
     }
 }
