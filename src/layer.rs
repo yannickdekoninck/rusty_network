30909795e26f8e8a3f92b548615c651aa6d141bc
Yannick De Koninck <yannick.dekoninck@gmail.com>
2022-10-23 10:56:52 +0200

    expanding the layer trait for backprop

--- src/layer.rs
+++ src/layer.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-use crate::network::Network;
+use crate::network::{Network, NetworkRunState};
 use crate::tensor::{helper::TensorShape, Tensor};
 
 pub mod convolutional;
@@ -50,7 +50,23 @@ impl SerializedLayer {
 }
 
 pub trait Layer {
-    fn forward(self: &Self, input: &Tensor, output: &mut Tensor);
+    fn forward(self: &mut Self, input: &Tensor, output: &mut Tensor) -> Result<(), &'static str>;
+    // Temporary blank impl so we can test these
+    fn backward(
+        self: &mut Self,
+        input: &Tensor,
+        incoming_gradient: &Tensor,
+        outgoing_gradient: &mut Tensor,
+    ) -> Result<(), &'static str> {
+        return Ok(());
+    }
+    fn apply_gradients(self: &mut Self) {}
+    fn clear_gradients(self: &mut Self) {}
+    fn switch_to_learning(self: &mut Self) {}
+    fn switch_to_inference(self: &mut Self) {}
+    fn get_run_mode(self: &Self) -> NetworkRunState {
+        return NetworkRunState::Inference;
+    }
     fn get_input_shape(self: &Self) -> TensorShape;
     fn get_output_shape(self: &Self) -> TensorShape;
     fn get_name(self: &Self) -> String;
